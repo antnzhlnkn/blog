59a0f15725644cba3a6084492b23de0ee3e08df9
antnzhlnkn <antnzhlnkn@users.noreply.github.com>
2020-08-17 15:05:47 +0000

    deployed via Gatsby Publish Action 🎩 for 9ef3a07f5194cd4d80941740c63c4c4627289d13

---
+++ component---src-pages-404-tsx-044acfb7c6f1f4939ed7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),o=a.n(n),r=a("8yLC"),i=a("H8eV"),c=a("ofer");e.default=function(){return o.a.createElement(r.a,{container:!0},o.a.createElement(i.a,{title:"404: Not found"}),o.a.createElement(c.a,{variant:"h2",gutterBottom:!0,component:"h1"},"NOT FOUND"),o.a.createElement(c.a,{variant:"body1"},"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-044acfb7c6f1f4939ed7.js.map
